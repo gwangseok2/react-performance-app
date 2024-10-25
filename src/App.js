325a6492186fd69155f53331fb3fca4e97c24459
golang <rhrnrkd135@gmail.com>
2024-10-25 22:14:49 +0900

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [value, setValue] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((posts) => setPosts(posts));
+  }, []);
+
+  return (
+    <div className="App" style={{ padding: "1rem" }}>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <ul>
+        {posts.map((el) => {
+          return <li key={el.id}>{el.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
